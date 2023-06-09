0b9bc02c7dfed7185e9b600aaa46d0e7685a1089
ratarby <ratarby@gmail.com>
2023-06-09 11:08:08 +0200

    fixed bugs

--- script/Page/tags.js
+++ script/Page/tags.js
@@ -1,49 +1,9 @@
 
-let tags = [];
-
-
-const handleClickTag = tags => {
-    tags= recipes.flatMap((x) => x.ingredients.map(y => y.ingredient.toLowerCase().normalize("NFD")));
-    tags= recipes.flatMap((x) => x.appliance.toLowerCase().normalize("NFD"));
-    tags= recipes.filter((x) => x.ustensils).flatMap((x) => x.ustensils.map((x) => x.toLowerCase().normalize("NFD")))
-    
-    tags.forEach(function (e) {
-        tags.push(e.target.dataset.name);
-        console.log(tags.push(e.target.dataset.name));
-        // Create new tag element.
-        const newTag = document.createElement('div');
-        newTag.className = 'tag';
-        newTag.textContent = e.target.dataset.name;
-    
-        // Create remove icon element.
-        const removeIcon = document.createElement('i');
-        removeIcon.className = 'far fa-times-circle';
-        removeIcon.dataset.name = e.target.dataset.name;
-    
-        // Append new tag and remove icon elements to the list of tags.
-        const tagsList = document.getElementById('tags');
-        newTag.appendChild(removeIcon);
-        tagsList.appendChild(newTag);
-    
-        // Update the recipe cards.
-    
-        displayRecipesCard(tags);
-        console.log(displayRecipesCard(tags));
-        });
-    };
-
-
-
-function displayTags() {
-    document.getElementById('drop').addEventListener('click', (e) => handleClickTag(e, tags));
-}
-
-
 
 displayIngredientsTags();
 
-let tagsIngredients = [];
 
+// console.log(tagsIngredients);
 function handleClickIngr(e, tagsIngredients) {
     // Add clicked ingredient to tagsIngredients' array
     tagsIngredients.push(sortByIngredients(e.target.dataset.name));
@@ -60,21 +20,20 @@ function handleClickIngr(e, tagsIngredients) {
 
     // Append new tag and remove icon elements to the list of tags.
     const tagsListIngr = document.getElementById('tagsIngr');
+    // console.log(tagsListIngr);
     newTagIngr.appendChild(removeIcon);
     tagsListIngr.appendChild(newTagIngr);
 
-    // Update the recipe cards
-    displayRecipesCard(tagsIngredients);
 }
 
 function displayIngredientsTags() {
     const tagsIngredients = recipes.flatMap((x) => x.ingredients.map(y => y.ingredient.toLowerCase().normalize("NFD")));
+    // console.log(tagsIngredients);
     document.getElementById('drop-ingredients_open').addEventListener('click', (e) => handleClickIngr(e, tagsIngredients));
 }
 
 displayAppliancesTags();
 
-let tagsAppliances = [];
 
 
 function handleClickAppl(e, tagsAppliances) {
@@ -94,22 +53,21 @@ function handleClickAppl(e, tagsAppliances) {
 
     // Append new tag and remove icon elements to the list of tags.
     const tagsListAppl = document.getElementById('tagsAppl');
+    // console.log(tagsListAppl);
     newTagAppl.appendChild(removeIcon);
     tagsListAppl.appendChild(newTagAppl);
 
-    // Update the recipe cards.
-    displayRecipesCard(tagsAppliances);
 }
 
 
 function displayAppliancesTags() {
     const tagsAppliances = recipes.flatMap((x) => x.appliance.toLowerCase().normalize("NFD"));
+    // console.log(tagsAppliances);
     document.getElementById('drop-appareil_open').addEventListener('click', (e) => handleClickAppl(e, tagsAppliances));
 }
 
 displayUstensilesTags();
 
-let tagsUstensiles = [];
 
 
 function handleClickUst(e, tagsUstensiles) {
@@ -128,11 +86,9 @@ function handleClickUst(e, tagsUstensiles) {
 
     // Append new tag and remove icon elements to the list of tags.
     const tagsListUst = document.getElementById('tagsUst');
+    // console.log(tagsListUst);
     newTagUst.appendChild(removeIcon);
     tagsListUst.appendChild(newTagUst);
-
-    // Update the recipe cards.
-    displayRecipesCard(tagsUstensiles);
 }
 
 function displayUstensilesTags() {
@@ -140,6 +96,7 @@ function displayUstensilesTags() {
         (x) => x.ustensils).flatMap( 
             (x) => x.ustensils.map(
                 (x) => x.toLowerCase().normalize("NFD")));
+    console.log(tagsUstensiles);
     document.getElementById('drop-ustensiles_open').addEventListener('click', (e) => handleClickUst(e, tagsUstensiles));
 }
 
